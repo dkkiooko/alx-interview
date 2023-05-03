5e1cf8b22783790388908b52988c6f74322ba900
dkkiooko <kithekadankioko@gmail.com>
2023-05-03 21:13:50 +0300

    changed the operationf from an if statement to a while loop to get the summation when it is perfectly divisible by the length of the argument

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,20 +4,11 @@
 
 def minOperations(n):
     """ find minimum number of operations """
-    if (n == 1):
-        return 0
-    if (n == 2):
-        return 2
-    count = 'HH'
-    operations = 2
-    paste = 'H'
-    while operations <= n:
-        if (n % len(count) == 0):
-            operations = operations + 2
-            paste = paste + paste
-            count = count + paste
-        else:
-            operations = operations + 1
-            count = count + paste
-        if len(count) == n:
-            return operations
+    minimum = 2
+    total = 0
+    while n > 1:
+        while n % minimum == 0:
+            total += minimum
+            n /= minimum
+        minimum += 1
+    return total
